f41a98298528dc2b8145f885d3882006e2686395
Stevo <steven.barrios92@gmail.com>
2022-09-22 15:09:35 -0700

    almost finished just gotta add bootstrrap

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+let mainBody = '';
 
 const Manager = require('./lib/Manager');
-const Employee = require('./lib/Employee');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 
@@ -48,8 +48,8 @@ inquirer
     .prompt(managerQuestions)
     .then((response) => {
         const GenManager = new Manager(response.name, response.id, response.email, response.officeNumber);
-        const createCard = cardTemplates.generateManagerCard(Manager);
-        mainBody += generateCard;
+        const createCard = generateManagerHTML(GenManager);
+        mainBody += createCard;
 
         nextMember();
     })
@@ -67,7 +67,7 @@ function nextMember(){
         if(response.addition === "yes"){
             askEngineer();
         } else {
-            writeHTMLFile();
+            generateHTMLFile();
         }
     });
 }
@@ -110,8 +110,8 @@ function createInternCard() {
         .prompt(internQuestions)
         .then((response) => {
             const GenIntern = new Intern(response.name, response.id, response.email, response.SchoolInfo);
-            const createCard = cardTemplates.createInternCard(Intern);
-            mainBody += generateCard;
+            const createCard = generateInternHTML(GenIntern);
+            mainBody += createCard;
     
             nextMember();
         })
@@ -124,10 +124,12 @@ function createEngineerCard() {
         .prompt(engineerQuestions)
         .then((response) => {
             const GenEngineer = new Engineer(response.name, response.id, response.email, response.GBuserInfo);
-            const createCard = cardTemplates.createEngineerCard(Engineer);
+            const createCard = generateEngineerHTML(GenEngineer);
+            mainBody += createCard;
         })
 }
-function createHTMLFile(){
+
+function generateHTMLFile(){
     const writeHTMLFile = `
     <!DOCTYPE html>
 <html lang="en">
@@ -139,15 +141,61 @@ function createHTMLFile(){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <title>Best Team Ever</title>
-
+    
 </head>
 <body>
-    
+
+    <${mainBody}>
 </body>
 </html>`
+// center the body on bootstrap
 
 fs.writeFile("index.html", writeHTMLFile, (err) =>
 err? console.log(err) : console.log("Success!!"))
 }
-questions();
 //notes from mini project on Node and OOP
+// card.html for everyone. 
+// create seperate function that returns html card strings
+function generateManagerHTML(manager) {
+    return `
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${manager.name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+
+    `
+}
+function generateEngineerHTML(engineer) {
+    return `
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${engineer.name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+
+    `
+}
+    function generateInternHTML(intern) {
+        return `
+        <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${intern.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+    
+        `
+    }
+questions();
